6f93cb2702b3e4d1b70a08e78e1849baee83a43d
Doan Trong Vinh <clonevinh95@gmail.com>
2024-07-25 16:24:42 +0700

    where, operator in mysql

---
+++ WHERECONDITION.sql
@@ -0,0 +1,7 @@
+Select * from employees
+-- where employee_id = 3;
+-- where hourly_pay <= 30;
+-- where employee_id != 5;
+-- where hire_date IS NULL;
+-- where hourly_pay between 16 and 28;
+where first_name like 'l%';
