e100a3faaf2c945e70f6540197fa3ecf1210df75
Doan Trong Vinh <clonevinh95@gmail.com>
2024-07-25 15:22:14 +0700

    Insert Data

---
+++ INSERT_INTO.sql
@@ -0,0 +1,8 @@
+-- insert into employees
+-- value (2, "Kakir", "Kole", 15.25, "2023-08-1"),
+-- 	  (3, "Ciris", "Ronaldo", 35.9, "2023-12-12"),
+--       (4, "Leo", "Messi", 27.4, "2022-06-10");
+	
+insert into employees (employee_id, first_name, last_name)
+value (5, "Neymar", "Santos"); 
+select * from employees;
